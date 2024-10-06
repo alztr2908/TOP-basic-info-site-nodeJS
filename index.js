5613f7ef038cc509b6ff463a8d3831a4e5d64a46
alztr2908@gmail.com <alztr2908@gmail.com>
2024-10-07 06:04:43 +0800

    migrate code to express

--- index.js
+++ index.js
@@ -1,72 +1,25 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-
+const express = require("express");
+const app = express();
+const path = require("path");
 const serverName = "localhost";
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true);
-
-  if (reqUrl.pathname === "/") {
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.statusCode = 404;
-      res.setHeader("content-type", {
-        "Content-Type": "text/html",
-        "Content-Length": data.length,
-      });
-      res.write(data);
-      res.end();
-    });
-  } else if (reqUrl.pathname === "/about") {
-    fs.readFile("about.html", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.statusCode = 404;
-      res.setHeader("content-type", {
-        "Content-Type": "text/html",
-        "Content-Length": data.length,
-      });
-      res.write(data);
-      res.end();
-    });
-  } else if (reqUrl.pathname === "/contact-me") {
-    fs.readFile("contact-me.html", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.statusCode = 404;
-      res.setHeader("content-type", {
-        "Content-Type": "text/html",
-        "Content-Length": data.length,
-      });
-      res.write(data);
-      res.end();
-    });
-  } else {
-    fs.readFile("notFound.html", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.statusCode = 404;
-      res.setHeader("content-type", {
-        "Content-Type": "text/html",
-        "Content-Length": data.length,
-      });
-      res.write(data);
-      res.end();
-    });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-server.listen(PORT, () => {
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname + "/contact-me.html"));
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname + "/notFound.html"));
+});
+
+app.listen(PORT, () => {
   console.log(`Server running at http://${serverName}:${PORT}`);
 });
